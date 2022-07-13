3b8b47a0368328148c94723b16db0396630d59ea
Jeferson <jefe.casagrande@gmail.com>
2022-07-13 12:29:57 -0300

    Projeto criado

---
+++ web/src/views/TarefasView.vue
@@ -0,0 +1,15 @@
+<!-- <script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script> -->
+
+<template>
+  <main>
+    <div class="underline">Tarefas</div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+div {
+  @apply bg-slate-500;
+}
+</style>
